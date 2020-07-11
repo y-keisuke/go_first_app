67efe86bf627d12e1078fdc22b0772bb4f2c1279
keisuke yamauchi <keisuke.yamauchi.321@gmail.com>
2020-07-11 20:20:08 +0900

    update ハンドラ関数indexを編集

--- ch02/chitchat/main.go
+++ ch02/chitchat/main.go
@@ -30,13 +30,26 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html",
-	}
-	templates := template.Must(template.ParseFiles(files...))
 	threads, err := data.Threads()
 	if err == nil {
+		_, err := session(w, r)
+		public_tmlp_files := []string{
+			"templates/layout.html",
+			"templates/public.navbar.html",
+			"templates/index.html",
+		}
+		private_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/private.navbar.html",
+			"templates/index.html",
+		}
+		var templates *template.Template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(private_tmlp_files...))
+		} else {
+			templates = template.Must(template.ParseFiles(public_tmlp_files...))
+		}
+
 		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }
