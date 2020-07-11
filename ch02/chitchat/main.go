9689af53baa3fc986fcc7fdaf0178dbf2cb72383
keisuke yamauchi <keisuke.yamauchi.321@gmail.com>
2020-07-11 23:11:49 +0900

    update genrateHTML作成に伴いindexリファクタリング

--- ch02/chitchat/main.go
+++ ch02/chitchat/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"text/template"
 )
 
 func main() {
@@ -33,23 +32,10 @@ func index(w http.ResponseWriter, r *http.Request) {
 	threads, err := data.Threads()
 	if err == nil {
 		_, err := session(w, r)
-		public_tmlp_files := []string{
-			"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html",
-		}
-		private_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/private.navbar.html",
-			"templates/index.html",
-		}
-		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(private_tmlp_files...))
+			generateHTML(writer, threads, "layout", "public/navbar", "index")
 		} else {
-			templates = template.Must(template.ParseFiles(public_tmlp_files...))
+			generateHTML(writer, threads, "layout", "private/navbar", "index")
 		}
-
-		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }
