29508657ddb702d6fadc8e3e70b014c44561827d
keisuke yamauchi <keisuke.yamauchi.321@gmail.com>
2020-07-19 09:19:07 +0900

    update form_file ファイルが一つだけならこっちが早い

--- ch04/06file_uploader/server.go
+++ ch04/06file_uploader/server.go
@@ -7,9 +7,7 @@ import (
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
+	file, _, err := r.FormFile("uploaded")
 	if err == nil {
 		data, err := ioutil.ReadAll(file)
 		if err == nil {
