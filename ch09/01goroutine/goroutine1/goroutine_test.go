0acdf2f31a45fde78b378644f7374abc5bf29603
keisuke yamauchi <keisuke.yamauchi.321@gmail.com>
2020-07-26 07:57:48 +0900

    add goroutin

---
+++ ch09/01goroutine/goroutine1/goroutine_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func BenchmarkPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print1()
+	}
+}
+
+func BenchmarkPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint1()
+	}
+}
+
+func TestPrint1(t *testing.T) {
+	print1()
+}
+
+func TestGoPrint1(t *testing.T) {
+	goPrint1()
+	time.Sleep(1 * time.Millisecond)
+}
+
+func TestGoPrint2(t *testing.T) {
+	goPrint2()
+	time.Sleep(1 * time.Millisecond)
+}
